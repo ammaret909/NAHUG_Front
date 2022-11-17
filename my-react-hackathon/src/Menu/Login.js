ca722014b7f722798fcfcdcdbf82c7a71c77aefd
ariya <ariya_watchara@cmu.ac.th>
2022-11-17 15:43:01 +0700

    login Signin

--- my-react-hackathon/src/Menu/Login.js
+++ my-react-hackathon/src/Menu/Login.js
@@ -1,6 +1,41 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+    const [email, setEmail] = useState([]);
+    const [password, setPassword] = useState([]);
+    const [token, setToken] = useState("");
+
+    const handChange = (fn) => {
+        return (event) => {
+            fn(event.target.value);
+        };
+    };
+
+    useEffect(() => {
+        if (!token) return
+        window.location.href = "/home";
+    }, [token]);
+
+    const onSubmit = async (e) => {
+        try {
+            e.preventDefault();
+            const token = await axios.post("http://localhost:8080/login", {
+                email,
+                password,
+            });
+            console.log(token);
+            localStorage.setItem("status", JSON.stringify(token.data.token));
+            setToken(JSON.parse(localStorage.getItem("status")));
+            window.location.href = "/home";
+        } catch (error) {
+            console.error(error);
+            // if (error instanceof AxiosError) {
+            //     console.error(error.message);
+            // }
+        }
+    };
     return (
         <>
             <div className="bg-h-egg d-flex row login-con justify-content-center align-items-center m-0 p-0">
@@ -9,7 +44,7 @@ export default function Login() {
                     <img src="image/testlogo1.png" className=" h-logo-login justify-content-center " alt="testlogo1.png"></img>
                 </div>
 
-                <div className="d-flex col-md-6 col-12 justify-content-md-start justify-content-center align-items-center ">
+                <form className="d-flex col-md-6 col-12 justify-content-md-start justify-content-center align-items-center " onSubmit={onSubmit}>
                     <div className="bg-h-orange2 rounded-3 shadow p-login col-md-9 col-xl-7 col-12">
 
                         <div className="form-group pb-1 text-egg  t-text ">
@@ -18,22 +53,22 @@ export default function Login() {
 
                         <div className="form-group pb-3 text-orange h-text ">
                             <label for="exampleInputEmail1 ">Username</label>
-                            <input type="email" className="form-control" id="inputEmail1" placeholder="Username" />
+                            <input type="email" className="form-control" id="inputEmail1" placeholder="Username" onChange={handChange(setEmail)} />
                         </div>
 
                         <div class="form-group pb-3 text-orange h-text">
                             <label for="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="inputPassword1" placeholder="Password" />
+                            <input type="password" className="form-control" id="inputPassword1" placeholder="Password" onChange={handChange(setPassword)} />
                         </div>
 
                         <div className="d-flex justify-content-center">
                             <button type="submit" className="btn-or pb-2 d-flex col-5 m-buttom justify-content-center fw-bold rounded-3">Login</button>
-                            <button type="submit" className="btn-or pb-2 d-flex col-5 justify-content-center fw-bold rounded-3">Sing in</button>
+                            <Link to="/singin" className="text-decoration-none text-light btn-or pb-2 d-flex col-5 justify-content-center fw-bold rounded-3">Sing in</Link>
                         </div>
 
 
                     </div>
-                </div>
+                </form >
 
             </div>
         </>
